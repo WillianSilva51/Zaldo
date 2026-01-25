7089236436f5f6e7fd03a9b1ac231e030922e773
Willian Silva <williansilva@alu.ufc.br>
2026-01-25 12:39:30 -0300

    Feat/dto (#2)
    * feat: Add validation support and DTOs for transaction creation
    
    - Added `spring-boot-starter-validation` dependency to `pom.xml`.
    - Introduced `CreateTransactionRequest` and `TransactionResponse` DTOs.
    - Implemented `TransactionMapper` for domain-DTO conversion.
    - Updated `TransactionController` to use validation and mapping.
    
    Signed-off-by: Willian Silva <williansilva@alu.ufc.br>
    
    * feat: Add "Get All Transactions" API endpoint
    
    - Introduced `ListTransactionUseCase` interface and `ListTransactionService` implementation.
    - Updated `TransactionController` to include a GET endpoint for retrieving all transactions.
    - Added Bru test for fetching all transactions.
    - Enhanced `Create Transaction` Bru test with assertions and pre-request script.
    
    Signed-off-by: Willian Silva <williansilva@alu.ufc.br>
    
    ---------
    
    Signed-off-by: Willian Silva <williansilva@alu.ufc.br>

--- zaldo-api/src/main/java/br/com/github/williiansilva51/zaldo/infrastructure/adapters/in/web/TransactionController.java
+++ zaldo-api/src/main/java/br/com/github/williiansilva51/zaldo/infrastructure/adapters/in/web/TransactionController.java
@@ -2,22 +2,44 @@ package br.com.github.williiansilva51.zaldo.infrastructure.adapters.in.web;
 
 import br.com.github.williiansilva51.zaldo.core.domain.Transaction;
 import br.com.github.williiansilva51.zaldo.core.ports.in.CreateTransactionUseCase;
+import br.com.github.williiansilva51.zaldo.core.ports.in.ListTransactionUseCase;
+import br.com.github.williiansilva51.zaldo.infrastructure.adapters.in.web.dto.request.CreateTransactionRequest;
+import br.com.github.williiansilva51.zaldo.infrastructure.adapters.in.web.dto.response.TransactionResponse;
+import br.com.github.williiansilva51.zaldo.infrastructure.adapters.in.web.mapper.TransactionMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
     private final CreateTransactionUseCase createTransactionUseCase;
+    private final ListTransactionUseCase listTransactionUseCase;
+    private final TransactionMapper transactionMapper;
 
     @PostMapping
-    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
-        Transaction created = createTransactionUseCase.execute(transaction);
+    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody @Valid CreateTransactionRequest request) {
+        Transaction domainObject = transactionMapper.toDomain(request);
+
+        Transaction transaction = createTransactionUseCase.execute(domainObject);
+
+        TransactionResponse created = transactionMapper.toResponse(transaction);
+
         return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponse>> getAllTransactions() {
+        List<TransactionResponse> responseList = listTransactionUseCase
+                .execute()
+                .stream()
+                .map(transactionMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(responseList);
     }
 }
