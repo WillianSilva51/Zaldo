f699d311513999e06b2186e1a5f5467cd9298369
Willian Silva <williansilva@alu.ufc.br>
2026-02-06 12:17:11 -0300

    feat: Add Telegram bot integration and environment configuration
    - Added `ZaldoTelegramBot` component utilizing Spring LongPolling for Telegram communication.
    - Introduced `.env-example` file to document environment variables (e.g., `TOKEN_TELEGRAM_BOT`, `POSTGRES_URL`).
    - Updated `application.yaml` to include new bot token configuration.
    - Added new dependencies in `pom.xml` for Telegram bot, dotenv support, and environment management.
    - Updated `.gitignore` to include `.env`.
    - Created `HELP.md` for project startup guidance and external references.
    
    Signed-off-by: Willian Silva <williansilva@alu.ufc.br>

---
+++ zaldo-api/src/main/java/br/com/github/williiansilva51/zaldo/infrastructure/adapters/in/telegram/ZaldoTelegramBot.java
@@ -0,0 +1,66 @@
+package br.com.github.williiansilva51.zaldo.infrastructure.adapters.in.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.List;
+
+@Component
+public class ZaldoTelegramBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+    private final String botToken;
+
+    public ZaldoTelegramBot(@Value("${api.security.token.bot}") String token) {
+        botToken = token;
+        telegramClient = new OkHttpTelegramClient(getBotToken());
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(List<Update> updates) {
+        LongPollingSingleThreadUpdateConsumer.super.consume(updates);
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(chatId)
+                    .text(message)
+                    .build();
+
+            try {
+                telegramClient.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        System.out.println("Registered bot running state is: " + botSession.isRunning());
+    }
+}
